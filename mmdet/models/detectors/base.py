1b554a5f9054f5fdcb38f9623bb2f0389e274c94
dazory <dazory@naver.com>
2022-04-01 16:50:57 +0900

    can use samples_per_gpu higher than 1

--- mmdet/models/detectors/base.py
+++ mmdet/models/detectors/base.py
@@ -250,22 +250,27 @@ class BaseDetector(BaseModule, metaclass=ABCMeta):
             p_clean, p_aug1, p_aug2 = F.softmax(logits_clean, dim=1), F.softmax(logits_aug1, dim=1), F.softmax(logits_aug2, dim=1)
             # expand dim of roi_head.bbox_head.fc_cls and fc_reg
             if len(p_clean.size()) == 2:
-                p_clean, p_aug1, p_aug2 = p_clean.reshape(batch_size, p_clean.size()[-2], p_clean.size()[-1]), \
-                                          p_aug1.reshape(batch_size, p_aug1.size()[-2], p_aug1.size()[-1]), \
-                                          p_aug2.reshape(batch_size, p_aug2.size()[-2], p_aug2.size()[-1])
+                p_clean, p_aug1, p_aug2 = p_clean.reshape(batch_size, -1, p_clean.size()[-1]), \
+                                          p_aug1.reshape(batch_size, -1, p_aug1.size()[-1]), \
+                                          p_aug2.reshape(batch_size, -1, p_aug2.size()[-1])
 
             # Clamp mixture distribution to avoid exploding KL divergence
             p_mixture = torch.clamp((p_clean + p_aug1 + p_aug2) / 3., 1e-7, 1).log()
             jsd_loss_tmp = (F.kl_div(p_mixture, p_clean, reduction='batchmean') +
                          F.kl_div(p_mixture, p_aug1, reduction='batchmean') +
                          F.kl_div(p_mixture, p_aug2, reduction='batchmean')) / 3.
-            jsd_loss += jsd_loss_tmp
+            jsd_loss += self.train_cfg.jsd_loss_parameter * jsd_loss_tmp
             if use_wandb:
-                wandb.log({"p_clean(" + layer_name + ")": p_clean})
-                wandb.log({"p_aug1(" + layer_name + ")": p_aug1})
-                wandb.log({"p_aug2(" + layer_name + ")": p_aug2})
-                wandb.log({"p_mixture(" + layer_name + ")": p_mixture})
-                wandb.log({"jsd_loss(" + layer_name + ")": jsd_loss_tmp})
+                try:
+                    wandb.log({"p_clean(" + layer_name + ")": p_clean})
+                    wandb.log({"p_aug1(" + layer_name + ")": p_aug1})
+                    wandb.log({"p_aug2(" + layer_name + ")": p_aug2})
+                    wandb.log({"p_mixture(" + layer_name + ")": p_mixture})
+                    wandb.log({"jsd_loss(" + layer_name + ")": jsd_loss_tmp})
+                except:
+                    print('self.features[layer_name].shape:', self.features[layer_name].shape)
+                    print('logits_clean:', logits_clean, ', logits_aug1:', logits_aug1, ', logits_aug2:')
+                    print('p_clean:', p_clean, ', p_aug1:', p_aug1, ', p_aug2:', p_aug2, 'p_mixture:', p_mixture, 'jsd_loss:', jsd_loss_tmp)
             jsd_loss = torch.clamp(jsd_loss, 0)
         if use_wandb:
             wandb.log({"jsd_loss": jsd_loss})
