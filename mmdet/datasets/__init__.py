e820dfac13fc8587c7173c7f8e09e98549e1e518
woojulee24 <bigtree123433@gmail.com>
2022-06-10 02:07:37 +0000

    dataset scannet initialized! 1. scannet dataset parsing, 2. scannet config file

--- mmdet/datasets/__init__.py
+++ mmdet/datasets/__init__.py
@@ -9,6 +9,7 @@ from .dataset_wrappers import (ClassBalancedDataset, ConcatDataset,
 from .deepfashion import DeepFashionDataset
 from .lvis import LVISDataset, LVISV1Dataset, LVISV05Dataset
 from .samplers import DistributedGroupSampler, DistributedSampler, GroupSampler
+from .scannet import ScannetDataset
 from .utils import (NumClassCheckHook, get_loading_pipeline,
                     replace_ImageToTensor)
 from .voc import VOCDataset
@@ -17,7 +18,7 @@ from .xml_style import XMLDataset
 
 __all__ = [
     'CustomDataset', 'XMLDataset', 'CocoDataset', 'DeepFashionDataset',
-    'VOCDataset', 'CityscapesDataset', 'LVISDataset', 'LVISV05Dataset',
+    'VOCDataset', 'CityscapesDataset', 'ScannetDataset', 'LVISDataset', 'LVISV05Dataset',
     'LVISV1Dataset', 'GroupSampler', 'DistributedGroupSampler',
     'DistributedSampler', 'build_dataloader', 'ConcatDataset', 'RepeatDataset',
     'ClassBalancedDataset', 'WIDERFaceDataset', 'DATASETS', 'PIPELINES',
