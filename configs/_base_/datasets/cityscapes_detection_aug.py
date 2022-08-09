ea27f5ff7937ee44cfb5d60a61e4f61553baae68
woojulee24 <bigtree123433@gmail.com>
2022-08-09 08:39:18 +0000

    config file for frame loss and aug loss

---
+++ configs/_base_/datasets/cityscapes_detection_aug.py
@@ -0,0 +1,85 @@
+# dataset settings
+dataset_type = 'CityscapesDataset'
+data_root = '/ws/data/cityscapes/'
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline1 = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
+    # dict(type='Resize', img_scale=[(2048, 800), (2048, 1024)], keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.0),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
+]
+train_pipeline2 = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
+    # dict(type='Resize', img_scale=[(2048, 800), (2048, 1024)], keep_ratio=True),
+    dict(type='RandomAffine',
+         max_rotate_degree=0.0,
+         max_translate_ratio=0.1,
+         scaling_ratio_range=(1.0, 1.5),
+         max_shear_degree=0.0,
+         border=(0, 0),
+         border_val=(114, 114, 114),
+         min_bbox_size=2,
+         min_area_ratio=0.2,
+         max_aspect_ratio=20,
+         bbox_clip_border=True,
+         skip_filter=True),
+    dict(type='RandomFlip', flip_ratio=0.0),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(2048, 1024),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+train1 = dict(
+        type=dataset_type,
+        ann_file=data_root +
+                     'annotations/instancesonly_filtered_gtFine_train.json',
+        img_prefix=data_root + 'leftImg8bit/train/',
+        pipeline=train_pipeline1
+        )
+train2 = dict(
+        type=dataset_type,
+        ann_file=data_root +
+                     'annotations/instancesonly_filtered_gtFine_train.json',
+        img_prefix=data_root + 'leftImg8bit/train/',
+        pipeline=train_pipeline2
+        )
+data = dict(
+    samples_per_gpu=1,
+    workers_per_gpu=1,
+    train=dict(
+        type='ConDataset',
+        datasets=[train1, train2]),
+    val=dict(
+        type=dataset_type,
+        ann_file=data_root +
+                 'annotations/instancesonly_filtered_gtFine_val.json',
+        img_prefix=data_root + 'leftImg8bit/val/',
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root +
+                 'annotations/instancesonly_filtered_gtFine_val.json',
+        img_prefix=data_root + 'leftImg8bit/val/',
+        pipeline=test_pipeline),)
+evaluation = dict(metric=['bbox'])
---
+++ configs/cityscapes/frame_robustness/faster_rcnn_r50_fpn_1x_cityscape_frame.py
@@ -0,0 +1,55 @@
+_base_ = [
+    '../../_base_/models/faster_rcnn_r50_fpn.py',
+    '../../_base_/datasets/cityscapes_detection_frame.py', '../../_base_/default_runtime.py'
+]
+model = dict(
+    backbone=dict(init_cfg=None),
+    neck=dict(
+        loss=dict(temper=1, add_act='softmax', loss_type='mse'),
+    ),
+    roi_head=dict(
+        bbox_head=dict(
+            type='Shared2FCBBoxHead',
+            in_channels=256,
+            fc_out_channels=1024,
+            roi_feat_size=7,
+            num_classes=8,
+            bbox_coder=dict(
+                type='DeltaXYWHBBoxCoder',
+                target_means=[0., 0., 0., 0.],
+                target_stds=[0.1, 0.1, 0.2, 0.2]),
+            reg_class_agnostic=False,
+            loss_cls=dict(
+                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
+            loss_bbox=dict(type='SmoothL1Loss', beta=1.0, loss_weight=1.0)),
+        train_cfg = dict(
+            wandb=dict(
+                log=dict(
+                    features_list=[],
+                    vars=['log_vars'],
+                ))))
+# optimizer
+# lr is set for a batch size of 8
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(grad_clip=None)
+# learning policy
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=500,
+    warmup_ratio=0.001,
+    # [7] yields higher performance than [6]
+    step=[7])
+runner = dict(
+    type='EpochBasedRunner', max_epochs=8)  # actual epoch = 8 * 8 = 64
+custom_hooks = [
+    dict(type='FeatureHook',
+         layer_list=['neck.fpn_convs.0.conv', 'neck.fpn_convs.1.conv',])
+]
+log_config = dict(interval=100,
+                  hooks=[
+                      dict(type='TextLoggerHook'),
+                  ])
+# For better, more stable performance initialize from COCO
+load_from = '/ws/data/cityscapes/pretrained/faster_rcnn_r50_fpn_1x_cityscapes_20200502-829424c0.pth'
+# load_from = 'https://download.openmmlab.com/mmdetection/v2.0/mask_rcnn/mask_rcnn_r50_fpn_1x_coco/mask_rcnn_r50_fpn_1x_coco_20200205-d4b0c5d6.pth'  # noqa
