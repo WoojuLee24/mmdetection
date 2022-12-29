ae41ac6534bc4a7289614fdb4ecd4ead05c9f88f
dazory <dazory@naver.com>
2022-12-29 21:07:07 +0900

    log additional_loss_ratio

--- mmdet/models/losses/ai28/cross_entropy_loss_plus.py
+++ mmdet/models/losses/ai28/cross_entropy_loss_plus.py
@@ -1875,10 +1875,12 @@ class CrossEntropyLossPlus(nn.Module):
                 self.wandb_features[f'additional_loss({self.wandb_name})'].append(loss_additional)
                 self.wandb_features[f'lam_additional_loss({self.wandb_name})'].append(
                     self.lambda_weight * loss_additional)
+                self.wandb_features[f'additional_loss_ratio({self.wandb_name})'].append(self.lambda_weight * loss_additional / loss_cls)
             else:
                 self.wandb_features[f'ce_loss({self.wandb_name})'] = loss_cls
                 self.wandb_features[f'additional_loss({self.wandb_name})'] = loss_additional
                 self.wandb_features[f'lam_additional_loss({self.wandb_name})'] = self.lambda_weight * loss_additional
+                self.wandb_features[f'additional_loss_ratio({self.wandb_name})'] = self.lambda_weight * loss_additional / loss_cls
 
             for key, value in p_distribution.items():
                 self.wandb_features[f'{key}({self.wandb_name})'] = value
