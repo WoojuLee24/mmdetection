a51d6b5a97f0d54aa28b82253649c56dee901452
dazory <dazory@naver.com>
2023-01-12 22:40:21 +0900

    fc_cont can have multiple linear(+relu) layers. contrastive loss plus can take normalized_input as an argument

--- mmdet/models/losses/ai28/contrastive_loss_plus.py
+++ mmdet/models/losses/ai28/contrastive_loss_plus.py
@@ -8,84 +8,19 @@ from .contrastive_loss import supcontrast
 from .divergence import kl_div
 
 
-class SupJSD(nn.Module):
-    def __init__(self, reduction='mean', use_softmax=False):
-        super(SupJSD, self).__init__()
-        self.reduction = reduction
-        self.use_softmax = use_softmax
-
-    def forward(self, logits_clean, logits_aug1, logits_aug2, labels=None, eps=1e-7):
-        if self.use_softmax:
-            logits_clean, logits_aug1, logits_aug2 = F.softmax(logits_clean, dim=1), \
-                                                     F.softmax(logits_aug1, dim=1), \
-                                                     F.softmax(logits_aug2, dim=1),
-        else: # F.normalize
-            logits_clean, logits_aug1, logits_aug2 = F.normalize(logits_clean, dim=1), \
-                                                     F.normalize(logits_aug1, dim=1), \
-                                                     F.normalize(logits_aug2, dim=1),
-
-        probs = torch.stack([logits_clean, logits_aug1, logits_aug2], dim=0) # (M, B, D)
-        probs = probs.reshape(-1, probs.shape[-1]) # (M*B, D)
-        labels = labels.repeat(3) # (M,)
-
-        weighted_kld_list = []
-        for i in range(len(probs)):
-            # Anchor
-            anchor = probs[i]
-
-            # Mixture of positives (with same label)
-            y_mask = torch.eq(labels, labels[i])
-            num_pos = torch.sum(y_mask)
-            y_mask = y_mask.unsqueeze(-1).expand(probs.size())
-
-            mixture = torch.sum(probs * y_mask, dim=0) / num_pos
-
-            # Calculate JSD
-            weighted_kld = kl_div(mixture.clamp(min=eps).log().unsqueeze(0),
-                                  anchor.unsqueeze(0),
-                                  reduction=self.reduction) / num_pos
-            weighted_kld_list.append(weighted_kld)
-
-        weighted_kld = torch.stack(weighted_kld_list, dim=0)
-        jsd = torch.sum(weighted_kld)
-        return jsd
-
-
-class SelfJSD(nn.Module):
-    def __init__(self, jsd_criterion, reduction='mean', use_softmax=False):
-        super(SelfJSD, self).__init__()
-        self.jsd_criterion = jsd_criterion
-        self.reduction = reduction
-        self.use_softmax = use_softmax
-
-    def forward(self, logits_clean, logits_aug1, logits_aug2, labels=None):
-        if self.use_softmax:
-            logits_clean, logits_aug1, logits_aug2 = F.softmax(logits_clean, dim=1), \
-                                                     F.softmax(logits_aug1, dim=1), \
-                                                     F.softmax(logits_aug2, dim=1),
-        else: # F.normalize
-            logits_clean, logits_aug1, logits_aug2 = F.normalize(logits_clean, dim=1), \
-                                                     F.normalize(logits_aug1, dim=1), \
-                                                     F.normalize(logits_aug2, dim=1),
-
-        prob_list = [logits_aug1, logits_aug2]
-        targets = logits_clean
-        jsd = self.jsd_criterion(prob_list, targets, reduction=self.reduction)
-
-        return jsd
-
-
 @LOSSES.register_module()
 class ContrastiveLossPlus(nn.Module):
 
     def __init__(self,
                  version,
                  loss_weight=1,
+                 normalized_input=True,
                  **kwargs):
         """ContrastiveLossPlus."""
         super(ContrastiveLossPlus, self).__init__()
         self.version = version
         self.loss_weight = loss_weight
+        self.normalized_input = normalized_input
         self.kwargs = kwargs
 
         if self.version in ['1.1']:
@@ -105,9 +40,8 @@ class ContrastiveLossPlus(nn.Module):
         """
         if len(cont_feats) == 0:
             return torch.zeros(1)
-
-        mask = None # TODO
-
+        if self.normalized_input:
+            cont_feats = F.normalize(cont_feats, dim=1)
 
         # loss_feat = self.loss_criterion(cont_feats, labels, label_weights,
         #                                 # TODO
