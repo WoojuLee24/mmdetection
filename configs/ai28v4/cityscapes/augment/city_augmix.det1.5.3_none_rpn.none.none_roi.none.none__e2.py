f2dfb8e200d5af2d8cde144df984c0c40f03e5ec
dazory <dazory@naver.com>
2023-02-02 19:53:29 +0900

    add AugMixDetv1.5.3

---
+++ configs/ai28v4/cityscapes/augment/city_augmix.det1.5.3_none_rpn.none.none_roi.none.none__e2.py
@@ -0,0 +1,130 @@
+_base_ = [
+    '/ws/external/configs/_base_/models/faster_rcnn_r50_fpn.py',
+    '/ws/external/configs/_base_/datasets/cityscapes_detection.py',
+    '/ws/external/configs/_base_/default_runtime.py'
+]
+#############
+### MODEL ###
+#############
+model = dict(
+    backbone=dict(init_cfg=None),
+    roi_head=dict(
+        bbox_head=dict(
+            type='Shared2FCBBoxHead',
+            in_channels=256,
+            fc_out_channels=1024,
+            roi_feat_size=7,
+            num_classes=8,
+            bbox_coder=dict(
+                type='DeltaXYWHBBoxCoder',
+                target_means=[0., 0., 0., 0.],
+                target_stds=[0.1, 0.1, 0.2, 0.2]),
+            reg_class_agnostic=False,
+            loss_cls=dict(
+                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
+            loss_bbox=dict(type='SmoothL1Loss', beta=1.0, loss_weight=1.0))),
+    train_cfg=dict(
+        wandb=dict(
+            log=dict(
+                features_list=[],
+                vars=['log_vars']),
+        ),
+        analysis_list=[
+            dict(type='loss_weight', outputs=dict()),
+            dict(type='bbox_head_loss',
+                 log_list=['acc_pos', 'acc_neg', 'acc_orig', 'consistency']),
+        ]
+    )
+)
+###############
+### DATASET ###
+###############
+custom_imports = dict(imports=['mmdet.datasets.pipelines.augmix_detection'], allow_failed_imports=False)
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', img_scale=[(2048, 800), (2048, 1024)], keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    ### AugMix ###
+    dict(type='AugMixDetection', num_views=1, version='1.5.2',
+         aug_severity=3, geo_severity=1, mixture_depth=-1, **img_norm_cfg,
+         num_bboxes=(3, 10), fillmode='img',
+         scales=(0.001, 0.1), ratios=(0.3, 1/0.3)),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])]
+data = dict(
+    samples_per_gpu=2, workers_per_gpu=4,
+    train=dict(dataset=dict(pipeline=train_pipeline)),
+)
+
+################
+### RUN TIME ###
+################
+# optimizer
+# lr is set for a batch size of 8
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(grad_clip=None)
+# learning policy
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=500,
+    warmup_ratio=0.001,
+    # [1] yields higher performance than [0]
+    step=[1])
+runner = dict(
+    type='EpochBasedRunner', max_epochs=2)  # actual epoch = 8 * 8 = 64
+
+###########
+### LOG ###
+###########
+custom_hooks = [
+    dict(type='FeatureHook',
+         layer_list=model['train_cfg']['wandb']['log']['features_list']),
+]
+
+train_version = 'v4'
+pipeline = 'augmix.det1.5.3'
+loss_type = 'none'
+rpn_loss = 'none.none'
+roi_loss = 'none.none'
+lambda_weight = ''
+
+name = f"{train_version}_{pipeline}.{loss_type}_rpn.{rpn_loss}_roi.{roi_loss}__e{str(runner['max_epochs'])}_lw.{lambda_weight}"
+
+print('++++++++++++++++++++')
+print(f"{name}")
+print('++++++++++++++++++++')
+
+log_config = dict(interval=100,
+                  hooks=[
+                      dict(type='TextLoggerHook'),
+                      dict(type='WandbLogger',
+                           wandb_init_kwargs={'project': "AI28v4", 'entity': "kaist-url-ai28",
+                                              'name': name,
+                                              'config': {
+                                                  # data pipeline
+                                                  'data pipeline': f"{pipeline}",
+                                                  # losses
+                                                  'loss type(rpn)': f"{rpn_loss}",
+                                                  'loss type(roi)': f"{roi_loss}",
+                                                  # parameters
+                                                  'epoch': runner['max_epochs'],
+                                                  'lambda_weight': lambda_weight,
+                                              }},
+                           interval=500,
+                           log_checkpoint=True,
+                           log_checkpoint_metadata=True,
+                           num_eval_images=5),
+                  ]
+                  )
+
+############
+### LOAD ###
+############
+# For better, more stable performance initialize from COCO
+load_from = 'https://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_1x_coco/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'  # noqa
