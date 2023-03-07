1b4cd781503cadbe3b70e199e6845874e4bab8d2
woojulee24 <bigtree123433@gmail.com>
2023-03-07 20:07:35 +0000

    2.7.5 final

---
+++ configs/ai28v4/pascal_voc/detf2.7.5_contv1.0_e4to6/voc_augmix.detf2.7.5_rpn.jsdv1.3.none_roi.jsdv1.3.none_contv1.0__e4_lw1e-2.20.0.py
@@ -0,0 +1,108 @@
+_base_ = [
+    '/ws/external/configs/_base_/models/faster_rcnn_r50_fpn.py',
+    '/ws/external/configs/_base_/datasets/voc0712.py',
+    '/ws/external/configs/_base_/default_runtime.py'
+]
+num_views=2
+model = dict(
+    rpn_head=dict(
+        loss_cls=dict(
+            type='CrossEntropyLossPlus', use_sigmoid=True, loss_weight=1.0, num_views=num_views,
+            additional_loss='jsdv1_3_2aug', lambda_weight=0.01, wandb_name='rpn_cls'),
+        loss_bbox=dict(type='L1LossPlus', loss_weight=1.0, num_views=num_views,
+                       additional_loss="None", lambda_weight=0.0001, wandb_name='rpn_bbox')),
+    roi_head=dict(
+        type='ContrastiveRoIHead',
+        bbox_head=dict(
+            num_classes=20,
+            type='Shared2FCContrastiveHead',
+            with_cont=True,
+            cont_predictor_cfg=dict(num_linear=2, feat_channels=256, return_relu=True),
+            out_dim_cont=256,
+            loss_cls=dict(
+                type='CrossEntropyLossPlus', use_sigmoid=False, loss_weight=1.0, num_views=num_views,
+                additional_loss='jsdv1_3_2aug', lambda_weight=20, wandb_name='roi_cls', log_pos_ratio=True),
+            loss_bbox=dict(type='L1LossPlus', loss_weight=1.0, num_views=num_views,
+                       additional_loss="None", lambda_weight=0.0001, wandb_name='roi_bbox'),
+            loss_cont=dict(type='ContrastiveLossPlus', version='1.0', loss_weight=0.0, num_views=num_views,
+                       memory=0, num_classes=21, dim=256, min_samples=5)
+    )),
+    train_cfg=dict(
+        wandb=dict(
+            log=dict(
+                features_list=[],
+                vars=['log_vars']),
+        ),
+        analysis_list=[]
+    )
+)
+
+###############
+### DATASET ###
+###############
+custom_imports = dict(imports=['mmdet.datasets.pipelines.augmix_detection_faster'], allow_failed_imports=False)
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', img_scale=(1000, 600), keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    ### AugMix ###
+    dict(type='AugMixDetectionFaster', num_views=num_views, version='2.7',
+         aug_severity=3, mixture_depth=-1, **img_norm_cfg,
+         num_bboxes=(3, 10), scales=(0.01, 0.2), ratios=(0.3, 1 / 0.3),
+         pre_blur=False, fillmode='var_blur', sigma_ratio=1 / 6,
+         mixture_width=1, ),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'img2',
+                               'gt_bboxes', 'gt_bboxes2',
+                               'gt_labels']),
+]
+data = dict(
+    samples_per_gpu=4, workers_per_gpu=4,
+    train=dict(dataset=dict(pipeline=train_pipeline))
+)
+
+################
+### RUN TIME ###
+################
+# optimizer
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(grad_clip=None)
+# learning policy
+# actual epoch = 3 * 3 = 9
+lr_config = dict(policy='step', step=[3])
+# runtime settings
+runner = dict(
+    type='EpochBasedRunner', max_epochs=6)  # actual epoch = 4 * 3 = 12
+
+###########
+### LOG ###
+###########
+custom_hooks = [
+    dict(type='FeatureHook',
+         layer_list=model['train_cfg']['wandb']['log']['features_list']),
+]
+
+train_version = 'v4'
+dataset = 'voc'
+pipeline = 'augmix.det3.7.5'
+loss_type = 'none'
+rpn_loss = 'none.none'
+roi_loss = 'none.none'
+lambda_weight = ''
+
+name = f"{train_version}_{dataset}_{pipeline}.{loss_type}_rpn.{rpn_loss}_roi.{roi_loss}__e{str(runner['max_epochs'])}_lw.{lambda_weight}"
+
+print('++++++++++++++++++++')
+print(f"{name}")
+print('++++++++++++++++++++')
+
+log_config = dict(interval=100,
+                  hooks=[
+                      dict(type='TextLoggerHook'),
+                  ]
+                  )
