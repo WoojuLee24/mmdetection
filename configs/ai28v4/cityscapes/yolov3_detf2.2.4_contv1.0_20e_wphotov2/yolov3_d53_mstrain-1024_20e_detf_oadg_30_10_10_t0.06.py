17cf8514199fa8cc3f24906865a9c5329311992d
woojulee24 <bigtree123433@gmail.com>
2023-05-30 05:51:17 +0000

    cfg

---
+++ configs/ai28v4/cityscapes/yolov3_detf2.2.4_contv1.0_20e_wphotov2/yolov3_d53_mstrain-1024_20e_detf_oadg_30_10_10_t0.06.py
@@ -0,0 +1,158 @@
+_base_ = [
+    '/ws/external/configs/_base_/default_runtime.py',
+    # '/ws/external/configs/_base_/datasets/cityscapes_detection.py',
+    ]
+num_views=2
+# model settings
+model = dict(
+    type='YOLOV3',
+    backbone=dict(
+        type='Darknet',
+        depth=53,
+        out_indices=(3, 4, 5),
+        init_cfg=None),
+        # init_cfg=dict(type='Pretrained', checkpoint='open-mmlab://darknet53')),
+    neck=dict(
+        type='YOLOV3Neck',
+        num_scales=3,
+        in_channels=[1024, 512, 256],
+        out_channels=[512, 256, 128]),
+    bbox_head=dict(
+        type='YOLOV3HeadCont',
+        num_classes=8,
+        in_channels=[512, 256, 128],
+        out_channels=[1024, 512, 256],
+        anchor_generator=dict(
+            type='YOLOAnchorGenerator',
+            base_sizes=[[(116, 90), (156, 198), (373, 326)],
+                        [(30, 61), (62, 45), (59, 119)],
+                        [(10, 13), (16, 30), (33, 23)]],
+            strides=[32, 16, 8]),
+        bbox_coder=dict(type='YOLOBBoxCoder'),
+        featmap_strides=[32, 16, 8],
+        loss_cls=dict(
+            type='CrossEntropyLoss',
+            use_sigmoid=True,
+            loss_weight=1.0,
+            reduction='sum'),
+        loss_conf=dict(
+            type='CrossEntropyLoss',
+            use_sigmoid=True,
+            loss_weight=1.0,
+            reduction='sum'),
+        loss_xy=dict(
+            type='CrossEntropyLoss',
+            use_sigmoid=True,
+            loss_weight=2.0,
+            reduction='sum'),
+        loss_wh=dict(type='MSELoss', loss_weight=2.0, reduction='sum'),
+        jsd_conf_weight=30.0,
+        jsd_cls_weight=10.0,
+        cont_cfg=dict(type='1.0', loss_weight=0.0, dim=256, temperature=0.06)),
+    # training and testing settings
+    train_cfg=dict(
+        assigner=dict(
+            type='GridAssigner',
+            pos_iou_thr=0.5,
+            neg_iou_thr=0.5,
+            min_pos_iou=0)),
+    test_cfg=dict(
+        nms_pre=1000,
+        min_bbox_size=0,
+        score_thr=0.05,
+        conf_thr=0.005,
+        nms=dict(type='nms', iou_threshold=0.45),
+        max_per_img=100))
+# dataset settings
+dataset_type = 'CityscapesDataset'
+data_root = '/ws/data/cityscapes/'
+img_norm_cfg = dict(mean=[0, 0, 0], std=[255., 255., 255.], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile', to_float32=True),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='Expand',
+        mean=img_norm_cfg['mean'],
+        to_rgb=img_norm_cfg['to_rgb'],
+        ratio_range=(1, 2)),
+    dict(
+        type='MinIoURandomCrop',
+        min_ious=(0.4, 0.5, 0.6, 0.7, 0.8, 0.9),
+        min_crop_size=0.3),
+    dict(type='Resize', img_scale=[(800, 800), (1024, 1024)], # [(480, 480), (608, 608)],
+         keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='PhotoMetricDistortion'), # include?
+    dict(type='AugMixDetectionFaster', num_views=num_views, version='2.2.4',
+         aug_severity=3, mixture_depth=-1, **img_norm_cfg,
+         num_bboxes=(3, 10), scales=(0.01, 0.2), ratios=(0.3, 1 / 0.3),
+         pre_blur=True, fillmode='var_blur', sigma_ratio=1 / 8, mixture_width=1, ),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'img2',
+                               'gt_bboxes', 'gt_bboxes2',
+                               'gt_labels'])
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(2048, 1024),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img'])
+        ])
+]
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=4,
+    train=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/instancesonly_filtered_gtFine_train.json',
+        img_prefix=data_root + 'leftImg8bit/train/',
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/instancesonly_filtered_gtFine_val.json',
+        img_prefix=data_root + 'leftImg8bit/val/',
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root +'annotations/instancesonly_filtered_gtFine_val.json',
+        img_prefix=data_root + 'leftImg8bit/val/',
+        pipeline=test_pipeline))
+# optimizer
+optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0005)
+optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
+# learning policy
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=500,  # cityscapes set
+    warmup_ratio=0.001,
+    step=[10, 15])
+# runtime settings
+runner = dict(type='EpochBasedRunner', max_epochs=20)
+evaluation = dict(interval=1, metric=['bbox'])
+checkpoint_config = dict(interval=1, max_keep_ckpts=5)
+
+load_from = "/ws/external/pretrained/yolov3_d53_mstrain-608_273e_coco_20210518_115020-a2c3acb8.pth"
+
+log_config = dict(
+    hooks=[
+    dict(type='TextLoggerHook'),
+    dict(type='WandbLogger',
+         wandb_init_kwargs={'project': "AI28v4", 'entity': "kaist-url-ai28",
+                            'name': "yolov3_d53_mstrain-1024_20e_detf_oadg_30_10_10_wphotov2_t0.06",
+                            },
+         log_map_every_iter=False,
+         log_checkpoint=True,
+         log_checkpoint_metadata=True
+         ),
+])
